cc5677a4e6db7de3d3b276d9bac58e6b789255e2
StevenAks21 <160478328+StevenAks21@users.noreply.github.com>
2025-07-07 17:27:15 +1000

    added API KEY
    added api key in a .env environment, and tested code, openapi request now works perfectly

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 from requests import get
 import time
+import openai
+from dotenv import load_dotenv
+import os
+
+# Initial setup
+load_dotenv()
+APIKey = os.getenv("API_KEY")
+openai.api_key = APIKey
+
+currTime = time.ctime(time.time())
+
+#Setting ChatGPT Up
+BotResponse = openai.responses.create(
+    model = 'gpt-4.1',
+    input = f'hi, guess whgere im from, its now exactly {currTime}'
+)
+
+print(BotResponse.output_text)
+print(currTime)
