be3b601d460f587ebc94957dc3797e7a647ed49c
StevenAks21 <160478328+StevenAks21@users.noreply.github.com>
2025-07-14 11:19:38 +1000

    added chatgpt proper API
    able to communicate to the API

--- main.py
+++ main.py
@@ -8,14 +8,17 @@ import os
 load_dotenv()
 APIKey = os.getenv("API_KEY")
 openai.api_key = APIKey
+Bot = openai.OpenAI(api_key=APIKey)
 
 currTime = time.ctime(time.time())
 
-#Setting ChatGPT Up
-BotResponse = openai.responses.create(
-    model = 'gpt-4.1',
-    input = f'hi, guess whgere im from, its now exactly {currTime}'
-)
+#Finding price of forex right now
+url = 'https://www.investing.com/currencies/eur-usd-historical-data'
+content = get(url).text
+price = float(re.findall(r'data-test="instrument-price-last">([\d.]+)</div>', content)[0])
 
-print(BotResponse.output_text)
-print(currTime)
+
+#Ask for AI Opinion
+response = Bot.responses.create(model= 'gpt-4.1', input='hi!, say something in indonesian')
+print(response.output_text)
+open('x.html', 'w', encoding='utf-8').write(content)
